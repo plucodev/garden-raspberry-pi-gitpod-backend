7997c1d7a7a7b2da967f3db7d80b8eb00e2803bb
Paolo Lucano <paololucanodeveloper@gmail.com>
2020-06-07 16:45:10 +0000

    fixed password issue

--- src/main.py
+++ src/main.py
@@ -7,8 +7,7 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap, sha256
-from models import db
-#from models import Person
+from models import db, User
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -28,14 +27,76 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@app.route('/user', methods=['POST', 'GET'])
+def handle_user():
+    """
+    Create user and retrieve all users
+    """
 
-    response_body = {
-        "hello": "world"
-    }
+    # POST request
+    if request.method == 'POST':
+        body = request.get_json()
 
-    return jsonify(response_body), 200
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", status_code=400)
+        if 'username' not in body:
+            raise APIException('You need to specify the username', status_code=400)
+        if 'email' not in body:
+            raise APIException('You need to specify the email', status_code=400)
+        if 'password' not in body:
+            raise APIException('You need to specify the password', status_code=400)    
+
+        user1 = User(username=body['username'], email=body['email'], password=sha256(body['password']))
+        db.session.add(user1)
+        db.session.commit()
+        return "ok", 200
+
+    # GET request
+    if request.method == 'GET':
+        all_users = User.query.all()
+        all_users = list(map(lambda x: x.serialize(), all_users))
+        return jsonify(all_users), 200
+
+    return "Invalid Method", 404
+
+
+@app.route('/user/<int:user_id>', methods=['PUT', 'GET', 'DELETE'])
+def get_single_user(user_id):
+    """
+    Single user
+    """
+    current_user = User.query.get(user_id)
+    # PUT request
+    if request.method == 'PUT':
+        body = request.get_json()
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", status_code=400)
+        if current_user is None:
+            raise APIException('User not found', status_code=404)
+        if "username" in body:
+            current_user.username = body["username"]
+        if "email" in body:
+            current_user.email = body["email"]
+        if "password" in body:
+            current_user.password = sha256(body["password"])
+        db.session.commit()
+
+        return jsonify(current_user.serialize()), 200
+
+    # GET request
+    if request.method == 'GET':
+        if current_user is None:
+            raise APIException('User not found', status_code=404)
+        return jsonify(current_user.serialize()), 200
+
+    # DELETE request
+    if request.method == 'DELETE':
+        if current_user is None:
+            raise APIException('User not found', status_code=404)
+        db.session.delete(current_user)
+        return "ok", 200
+
+    return "Invalid Method", 404
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -6,7 +6,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(10), unique=True, nullable=False)
+    password = db.Column(db.String(1256), unique=True, nullable=False)
+    role = db.Column(db.String(80), unique=False, nullable=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -16,5 +17,6 @@ class User(db.Model):
             "username": self.username,
             "email": self.email,
             "password": self.password,
+            "role": self.role,
             "id": self.id
         }
